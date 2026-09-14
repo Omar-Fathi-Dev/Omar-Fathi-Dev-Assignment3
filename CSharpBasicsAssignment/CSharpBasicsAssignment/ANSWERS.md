c1d89ce99f1ff5f1421b83b5a0a5430c80663683
Omar Fathi <its.omar.fathy@gmail.com>
2026-09-14 22:38:15 +0300

    Complete Part A - Project and Structure

---
+++ CSharpBasicsAssignment/CSharpBasicsAssignment/ANSWERS.md
@@ -0,0 +1,19 @@
+# 1 - `.csproj` Contents
+
+The `.csproj` file contains the **project configuration and settings**.
+
+```xml
+<Project Sdk="Microsoft.NET.Sdk">
+
+    <PropertyGroup>
+        <OutputType>Exe</OutputType>
+        <TargetFramework>net10.0</TargetFramework>
+        <ImplicitUsings>enable</ImplicitUsings>
+        <Nullable>enable</Nullable>
+    </PropertyGroup>
+
+</Project>
+```
+
+
+
---
+++ CSharpBasicsAssignment/CSharpBasicsAssignment/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace CSharpBasicsAssignment;
+
+#region  Part A — Project & Structure
+
+    #region the role of: .csproj, Program.cs, obj/, and bin/.
+
+        /*
+         *.csprog : it contains project configuration it specifies 1- target framework.
+         *          2- enable and disable features. 3- version of packages.
+         *
+         * program,cs : it has the entry point of the project where the program starts running.
+         *
+         * obj/ : It stores temporary files used during build.
+         *
+         * bin/ : It stores the final built files that the program can run
+         * 
+         */
+
+    #endregion
+
+    #region why file-scoped namespace removes a level of indentation?
+
+    /*
+     * It removes a level of indentation
+     * because we no longer need the curly braces { } to wrap the entire file.
+     */
+
+    #endregion
+
+    #region a classic .sln or the newer .slnx format,
+
+    /*
+     * I am using the classic .sln format.
+     * One advantage of .slnx is that it uses XML,
+     * which makes it more readable than the classic .sln format.
+     */
+
+    #endregion
+
+#endregion
+
+
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+
+    
+}
